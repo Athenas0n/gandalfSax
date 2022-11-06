d357e51fc849897e88c3a02837559e2233cc083a
swoledemort <lukemccullough1701@outlook.com>
2022-11-06 22:00:51 +0000

    Merge remote-tracking branch 'origin/Main' into index

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ const navSlide = () => {
 window.onload = () => navSlide();
 
 
+
+
+
+
+// Dark/Light mode
+const r = document.querySelector(':root');
+
+// Create a function for getting a variable value
+function getRValue() {
+    // Get the styles (properties and values) for the root
+    const rs = getComputedStyle(r);
+    // Alert the value of the --blue variable
+    alert("The value of --main-background is: " + rs.getPropertyValue('--main-background'));
+}
+
+// Create a function for setting a variable value
+function setRValues() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--main-background', 'lightblue');
+}
+
+
